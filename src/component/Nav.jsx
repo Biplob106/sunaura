2750af7c82ccf9fe3cf31953d524b84f057574b5
Biplob106 <biplobsd106@gmail.com>
2026-05-02 18:14:22 +0600

    nav issue fix

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,35 +1,28 @@
+import Link from "next/link";
+import NavLink from "./NavLink";
 
 const Nav = () => {
   return (
     <div>
         <div className="navbar bg-base-100 shadow-sm">
   <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Home</a></li>
-        <li>
-          <a>Products</a>
-        </li>
-        <li><a>Profile</a></li>
-      </ul>
-    </div>
+  
     <a className="btn btn-ghost text-xl">daisyUI</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
+      <li><NavLink href={"/"}>Home</NavLink></li>
       <li>
-        <a>Products</a>
+        <NavLink href={"/products"}>Products</NavLink>
       </li>
-      <li><a>Profile</a></li>
+      <li><NavLink href = {"/profile"}> Profile</NavLink></li>
     </ul>
   </div>
   <div className="navbar-end">
+    <div className="flex gap-4 my-4">
+        <h2>signin</h2>
+        <h2>signup</h2>
+    </div>
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
@@ -38,18 +31,7 @@ const Nav = () => {
             src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
         </div>
       </div>
-      <ul
-        tabIndex="-1"
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
+   
     </div>
   </div>
 </div>
